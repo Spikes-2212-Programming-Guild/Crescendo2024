4db730768b7ac9970c86132abd57dfc19a25f103
Programming Guild <tmp@tmp.com>
2024-01-18 21:47:30 +0200

    v0.000000001

--- src/main/java/frc/robot/command/Shoot.java
+++ src/main/java/frc/robot/command/Shoot.java
@@ -1,21 +1,22 @@
 package frc.robot.command;
 
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ShooterAdjuster;
 import frc.robot.subsystems.Storage;
 
-public class Shoot {
+public class Shoot extends SequentialCommandGroup {
 
     public Shoot(ShooterAdjuster shooterAdjuster, Drivetrain drivetrain, Storage storage) {
 
     }
 
-    public double getRequireShooterAngel()
+    public double getRequireShooterAngle()
     {
         return 0.0;
     }
 
-    public double getRequireRobotAngel()
+    public double getRequireRobotAngle()
     {
         return 0.0;
     }
