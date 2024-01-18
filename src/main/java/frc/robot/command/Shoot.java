2d76a73fddd9e9e6d9dcdc5411f21a05f012cb74
Programming Guild <tmp@tmp.com>
2024-01-18 21:43:55 +0200

    v0.000000001

---
+++ src/main/java/frc/robot/command/Shoot.java
@@ -0,0 +1,23 @@
+package frc.robot.command;
+
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.ShooterAdjuster;
+import frc.robot.subsystems.Storage;
+
+public class Shoot {
+
+    public Shoot(ShooterAdjuster shooterAdjuster, Drivetrain drivetrain, Storage storage) {
+
+    }
+
+    public double getRequireShooterAngel()
+    {
+        return 0.0;
+    }
+
+    public double getRequireRobotAngel()
+    {
+        return 0.0;
+    }
+
+}
