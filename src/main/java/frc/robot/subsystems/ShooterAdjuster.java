86a186d34d2e0535fe61eee9aeeaa51a283ce922
nettahalamish <bygneta1611@gmail.com>
2024-01-20 20:45:26 +0200

    created ShooterAdjuster

--- src/main/java/frc/robot/subsystems/ShooterAdjuster.java
+++ src/main/java/frc/robot/subsystems/ShooterAdjuster.java
@@ -1,17 +1,22 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkBase;
+import com.revrobotics.CANSparkMax;
 import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SparkGenericSubsystem;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 
 public class ShooterAdjuster extends SparkGenericSubsystem {
-    /**
-     * Constructs a new instance of {@link SparkGenericSubsystem}.
-     *
-     * @param namespaceName the name of the subsystem's namespace
-     * @param master        the motor controller which runs the loops
-     * @param slaves        additional motor controllers that follow the master
-     */
-    public ShooterAdjuster(String namespaceName, CANSparkBase master, CANSparkBase... slaves) {
-        super(namespaceName, master, slaves);
+
+    private static String NAMESPACE_NAME;
+    private static final double ENCODER_OFFSET = 0;
+
+    private final DutyCycleEncoder absoluteEncoder;
+
+    public ShooterAdjuster(CANSparkMax left, CANSparkMax right, DutyCycleEncoder absoluteEncoder) {
+        super(NAMESPACE_NAME, left, right);
+        this.absoluteEncoder = absoluteEncoder;
+    }
+
+    public double getAngle() {
+        return absoluteEncoder.get() * 360 + ENCODER_OFFSET;
     }
 }
