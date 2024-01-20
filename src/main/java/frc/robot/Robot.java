4c437244ed04bd0123cdab8353f1584cd7db9bc1
Spikes <Spikes.no2212@gmail.com>
2024-01-20 17:25:53 +0200

    SAVING BPLEROG ERWEAE

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,68 +4,86 @@
 
 package frc.robot;
 
+import com.spikes2212.util.PlaystationControllerWrapper;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.commands.DriveSwerve;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.DrivetrainImpl;
 
 public class Robot extends TimedRobot {
 
-  @Override
-  public void robotInit() {
+    private final PlaystationControllerWrapper ps = new PlaystationControllerWrapper(0);
+    private DrivetrainImpl drivetrain;
 
-  }
+    @Override
+    public void robotInit() {
+        drivetrain = DrivetrainImpl.getInstance();
+        drivetrain.resetRelativeEncoders();
+        drivetrain.configureDashboard();
+    }
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+        drivetrain.periodic();
+    }
 
-  @Override
-  public void disabledInit() {
+    @Override
+    public void disabledInit() {
 
-  }
+    }
 
-  @Override
-  public void disabledPeriodic() {
+    @Override
+    public void disabledPeriodic() {
 
-  }
+    }
 
-  @Override
-  public void autonomousInit() {
+    @Override
+    public void autonomousInit() {
+        DriveSwerve driveSwerve = new DriveSwerve(drivetrain, () -> 2.0, () -> 0.0, () -> 0.0, false, false);
+        driveSwerve.schedule();
+    }
 
-  }
+    @Override
+    public void autonomousPeriodic() {
 
-  @Override
-  public void autonomousPeriodic() {
+    }
 
-  }
+    @Override
+    public void teleopInit() {
+        DriveSwerve driveSwerve = new DriveSwerve(drivetrain,
+                () -> ps.getLeftY() * DrivetrainImpl.MAX_SPEED_METERS_PER_SECONDS,
+                () -> ps.getLeftX() * DrivetrainImpl.MAX_SPEED_METERS_PER_SECONDS,
+                () -> ps.getRightX() * DrivetrainImpl.MAX_SPEED_METERS_PER_SECONDS  ,
+                true, false);
+        drivetrain.setDefaultCommand(driveSwerve);
+        ps.getR1Button().onTrue(new InstantCommand(() -> drivetrain.resetGyro()));
+    }
 
-  @Override
-  public void teleopInit() {
+    @Override
+    public void teleopPeriodic() {
 
-  }
+    }
 
-  @Override
-  public void teleopPeriodic() {
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
 
-  }
+    @Override
+    public void testPeriodic() {
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    }
 
-  @Override
-  public void testPeriodic() {
+    @Override
+    public void simulationInit() {
 
-  }
+    }
 
-  @Override
-  public void simulationInit() {
+    @Override
+    public void simulationPeriodic() {
 
-  }
-
-  @Override
-  public void simulationPeriodic() {
-
-  }
+    }
 }
