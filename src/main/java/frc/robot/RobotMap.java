f953963df544b0ea2984321c0bf0d3ce1f968943
Programming Guild <tmp@tmp.com>
2024-01-20 17:41:48 +0200

    created IntakePlacer

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,10 +4,13 @@ public class RobotMap {
 
     public interface CAN {
 
+        int INTAKE_PLACER_SPARKMAX = -1;
     }
     
     public interface DIO {
 
+        int INTAKE_PLACER_TOP_LIMIT_SWITCH = -1;
+        int INTAKE_PLACER_BOTTOM_LIMIT_SWITCH = -1;
     }
     
     public interface PWM {
---
+++ src/main/java/frc/robot/subsystems/IntakePlacer.java
@@ -0,0 +1,33 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkLowLevel;
+import com.revrobotics.CANSparkMax;
+import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SparkGenericSubsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.RobotMap;
+
+public class IntakePlacer extends SparkGenericSubsystem {
+
+    private static final String NAMESPACE_NAME = "intake placer";
+
+    private final DigitalInput topLimitSwitch;
+    private final DigitalInput bottomLimitSwitch;
+
+    private static IntakePlacer instance;
+
+    public static IntakePlacer getInstance() {
+        if (instance == null) {
+            instance = new IntakePlacer(
+                    new CANSparkMax(RobotMap.CAN.INTAKE_PLACER_SPARKMAX, CANSparkLowLevel.MotorType.kBrushless),
+                    new DigitalInput(RobotMap.DIO.INTAKE_PLACER_TOP_LIMIT_SWITCH),
+                    new DigitalInput(RobotMap.DIO.INTAKE_PLACER_BOTTOM_LIMIT_SWITCH));
+        }
+        return instance;
+    }
+
+    private IntakePlacer(CANSparkMax motor, DigitalInput topLimitSwitch, DigitalInput bottomLimitSwitch) {
+        super(NAMESPACE_NAME, motor);
+        this.topLimitSwitch = topLimitSwitch;
+        this.bottomLimitSwitch = bottomLimitSwitch;
+    }
+}
