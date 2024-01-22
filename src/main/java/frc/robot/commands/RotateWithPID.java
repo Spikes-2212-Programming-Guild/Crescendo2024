46b23829ab97d09df25ce2cd75443be76a8a3399
nettahalamish <bygneta1611@gmail.com>
2024-01-22 15:18:26 +0200

    changed field name to setpoint

--- src/main/java/frc/robot/commands/RotateWithPID.java
+++ src/main/java/frc/robot/commands/RotateWithPID.java
@@ -11,20 +11,20 @@ import java.util.function.Supplier;
 public class RotateWithPID extends Command {
 
     private final Drivetrain drivetrain;
-    private final Supplier<Double> targetAngle;
+    private final Supplier<Double> setpoint;
     private final Supplier<Double> sourceAngle;
     private final PIDSettings pidSettings;
     private final PIDController pidController;
 
     private double lastTimeNotOnTarget;
 
-    public RotateWithPID(Drivetrain drivetrain, Supplier<Double> targetAngle, Supplier<Double> sourceAngle,
+    public RotateWithPID(Drivetrain drivetrain, Supplier<Double> setpoint, Supplier<Double> sourceAngle,
                          PIDSettings pidSettings) {
         addRequirements(drivetrain);
         this.drivetrain = drivetrain;
         this.pidController = new PIDController(pidSettings.getkP(), pidSettings.getkI(), pidSettings.getkD());
         pidController.setTolerance(pidSettings.getTolerance());
-        this.targetAngle = targetAngle;
+        this.setpoint = setpoint;
         this.sourceAngle = sourceAngle;
         this.pidSettings = pidSettings;
         lastTimeNotOnTarget = Timer.getFPGATimestamp();
@@ -38,7 +38,7 @@ public class RotateWithPID extends Command {
 
     @Override
     public void execute() {
-        drivetrain.drive(0, 0, pidController.calculate(targetAngle.get(), sourceAngle.get()), false, false);
+        drivetrain.drive(0, 0, pidController.calculate(setpoint.get(), sourceAngle.get()), false, false);
     }
 
     @Override
