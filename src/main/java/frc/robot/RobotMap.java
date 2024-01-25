713e46a075db91fab006df04b7953102f8bd1932
Programming Guild <tmp@tmp.com>
2024-01-25 18:08:18 +0200

    added limitswitches and made ShooterAdjuster singleton

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,10 +4,15 @@ public class RobotMap {
 
     public interface CAN {
 
+        int SHOOTER_ADJUSTER_LEFT_SPARKMAX = Integer.MAX_VALUE;
+        int SHOOTER_ADJUSTER_RIGHT_SPARKMAX = Integer.MAX_VALUE;
     }
     
     public interface DIO {
 
+        int SHOOTER_ADJUSTER_ABSOLUTE_ENCODER = Integer.MIN_VALUE;
+        int SHOOTER_ADJUSTER_TOP_LIMIT = Integer.MIN_VALUE;
+        int SHOOTER_ADJUSTER_BOTTOM_LIMIT = Integer.MIN_VALUE;
     }
     
     public interface PWM {
--- src/main/java/frc/robot/subsystems/ShooterAdjuster.java
+++ src/main/java/frc/robot/subsystems/ShooterAdjuster.java
@@ -1,8 +1,11 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkBase;
 import com.revrobotics.CANSparkMax;
 import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SparkGenericSubsystem;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import frc.robot.RobotMap;
 
 public class ShooterAdjuster extends SparkGenericSubsystem {
 
@@ -11,9 +14,31 @@ public class ShooterAdjuster extends SparkGenericSubsystem {
 
     private final DutyCycleEncoder absoluteEncoder;
 
-    public ShooterAdjuster(CANSparkMax left, CANSparkMax right, DutyCycleEncoder absoluteEncoder) {
+    private final DigitalInput topLimit;
+    private final DigitalInput bottomLimit;
+
+    private static ShooterAdjuster instance;
+
+    public static ShooterAdjuster getInstance() {
+        if (instance == null) {
+            instance = new ShooterAdjuster(
+                    new CANSparkMax(RobotMap.CAN.SHOOTER_ADJUSTER_LEFT_SPARKMAX,
+                            CANSparkBase.MotorType.kBrushless),
+                    new CANSparkMax(RobotMap.CAN.SHOOTER_ADJUSTER_RIGHT_SPARKMAX,
+                            CANSparkBase.MotorType.kBrushless),
+                    new DutyCycleEncoder(RobotMap.DIO.SHOOTER_ADJUSTER_ABSOLUTE_ENCODER),
+                    new DigitalInput(RobotMap.DIO.SHOOTER_ADJUSTER_TOP_LIMIT),
+                    new DigitalInput(RobotMap.DIO.SHOOTER_ADJUSTER_BOTTOM_LIMIT));
+        }
+        return instance;
+    }
+
+    private ShooterAdjuster(CANSparkMax left, CANSparkMax right, DutyCycleEncoder absoluteEncoder,
+                            DigitalInput topLimit, DigitalInput bottomLimit) {
         super(NAMESPACE_NAME, left, right);
         this.absoluteEncoder = absoluteEncoder;
+        this.topLimit = topLimit;
+        this.bottomLimit = bottomLimit;
     }
 
     public double getAngle() {
