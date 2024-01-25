3c5a991bc519e666b2cae580a4b3aad7b9e54da6
Programming Guild <tmp@tmp.com>
2024-01-25 20:08:15 +0200

    made robotmap boring also updated to java 17

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,15 +4,15 @@ public class RobotMap {
 
     public interface CAN {
 
-        int SHOOTER_ADJUSTER_LEFT_SPARKMAX = Integer.MAX_VALUE;
-        int SHOOTER_ADJUSTER_RIGHT_SPARKMAX = Integer.MAX_VALUE;
+        int SHOOTER_ADJUSTER_LEFT_SPARKMAX = -1;
+        int SHOOTER_ADJUSTER_RIGHT_SPARKMAX = -1;
     }
     
     public interface DIO {
 
-        int SHOOTER_ADJUSTER_ABSOLUTE_ENCODER = Integer.MIN_VALUE;
-        int SHOOTER_ADJUSTER_TOP_LIMIT = Integer.MIN_VALUE;
-        int SHOOTER_ADJUSTER_BOTTOM_LIMIT = Integer.MIN_VALUE;
+        int SHOOTER_ADJUSTER_ABSOLUTE_ENCODER = -1;
+        int SHOOTER_ADJUSTER_TOP_LIMIT = -1;
+        int SHOOTER_ADJUSTER_BOTTOM_LIMIT = -1;
     }
     
     public interface PWM {
