da676dee70f0f212f9fa934b98acde09cdca8e2c
nettahalamish <bygneta1611@gmail.com>
2024-01-26 10:01:12 +0200

    added getPidSettings and getFeedForwardSettings

--- src/main/java/frc/robot/subsystems/IntakePlacer.java
+++ src/main/java/frc/robot/subsystems/IntakePlacer.java
@@ -3,12 +3,18 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkLowLevel;
 import com.revrobotics.CANSparkMax;
 import com.spikes2212.command.genericsubsystem.smartmotorcontrollersubsystem.SparkGenericSubsystem;
+import com.spikes2212.control.FeedForwardSettings;
+import com.spikes2212.control.PIDSettings;
 import edu.wpi.first.wpilibj.DigitalInput;
 import frc.robot.RobotMap;
 
 public class IntakePlacer extends SparkGenericSubsystem {
 
     private static final String NAMESPACE_NAME = "intake placer";
+
+    private final PIDSettings pidSettings = namespace.addPIDNamespace("", PIDSettings.EMPTY_PID_SETTINGS);
+    private final FeedForwardSettings feedForwardSettings = namespace.addFeedForwardNamespace("",
+            FeedForwardSettings.EMPTY_FFSETTINGS);
 
     private final DigitalInput topLimitSwitch;
     private final DigitalInput bottomLimitSwitch;
@@ -37,5 +43,13 @@ public class IntakePlacer extends SparkGenericSubsystem {
 
     public boolean intakeDown() {
         return bottomLimitSwitch.get();
+    }
+
+    public PIDSettings getPidSettings() {
+        return pidSettings;
+    }
+
+    public FeedForwardSettings getFeedForwardSettings() {
+        return feedForwardSettings;
     }
 }
