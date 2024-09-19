8bee28934b7a7727cff089f2ab7bad04748d2cc1
turtleminecraft <schnitzelgirl69@gmail.com>
2024-09-19 12:09:59 +0300

    magic numbers, plus removal of unnecessary command

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,7 +7,12 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 
+import java.util.function.Supplier;
+
 public class OI /*GEVALD*/ {
+
+    private static final double STORAGE_RETRACTION_SPEED = 0.2;
+    private static final Supplier<Double> SHOOTER_RETRACTION_SPEED_ROTATIONS_PER_MINUTE = () -> -2000.0;
 
     private final PlaystationControllerWrapper ps = new PlaystationControllerWrapper(0);
     private final XboxControllerWrapper xbox = new XboxControllerWrapper(1);
@@ -29,13 +34,13 @@ public class OI /*GEVALD*/ {
         // Shoots the note from the safe zone - X
         xbox.getBlueButton().onTrue(new Shoot(shooter, drivetrain, shooterAdjuster, storage, Shoot.RECTANGLE_HEIGHT).getCommand());
         // Moves the storage backwards - LT
-        xbox.getLTButton().whileTrue(new MoveGenericSubsystem(storage, 0.2));
+        xbox.getLTButton().whileTrue(new MoveGenericSubsystem(storage, STORAGE_RETRACTION_SPEED));
         // Resets the adjuster - RT
-        xbox.getRTButton().whileTrue(new SpeedUpShooter(shooter, () -> -2000.0, () -> -2000.0));
+        xbox.getRTButton().whileTrue(new SpeedUpShooter(shooter, SHOOTER_RETRACTION_SPEED_ROTATIONS_PER_MINUTE,
+                SHOOTER_RETRACTION_SPEED_ROTATIONS_PER_MINUTE));
         // Stops all running commands - Left
         xbox.getUpButton().onTrue(new CloseIntake(intakePlacer));
         xbox.getDownButton().onTrue(new OpenIntake(intakePlacer));
-        xbox.getRightButton().whileTrue(new MoveGenericSubsystem(intakeRoller, 0.85).alongWith(new MoveGenericSubsystem(storage, 0.15)));
         xbox.getLeftButton().onTrue(new InstantCommand(() -> {
         }, storage, intakeRoller, shooterAdjuster, shooter) {
             @Override
