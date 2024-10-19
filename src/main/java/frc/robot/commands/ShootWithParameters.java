d8fb228bcff336e806f443794851c609eb01db88
TurtleMinecraft <schnitzelgirl69@gmail.com>
2024-10-20 00:29:08 +0300

    ctrl alt o

--- src/main/java/frc/robot/commands/ShootWithParameters.java
+++ src/main/java/frc/robot/commands/ShootWithParameters.java
@@ -4,8 +4,6 @@ import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.MoveSmartMotorControllerGenericSubsystem;
 import com.spikes2212.dashboard.RootNamespace;
 import com.spikes2212.util.UnifiedControlMode;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Shooter;
