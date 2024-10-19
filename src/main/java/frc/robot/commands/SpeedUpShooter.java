faae983d7c6f0275751c64941186687cef6f4f2f
TurtleMinecraft <schnitzelgirl69@gmail.com>
2024-10-20 00:29:37 +0300

    line breaks to not cross line

--- src/main/java/frc/robot/commands/SpeedUpShooter.java
+++ src/main/java/frc/robot/commands/SpeedUpShooter.java
@@ -14,14 +14,16 @@ public class SpeedUpShooter extends ParallelCommandGroup {
 
     public SpeedUpShooter(Shooter shooter, Supplier<Double> leftSetpoint, Supplier<Double> rightSetpoint) {
         addCommands(
-                new MoveSmartMotorControllerGenericSubsystem(shooter.getLeftFlywheel(), shooter.getLeftFlywheel().pidSettings,
-                        shooter.getLeftFlywheel().feedForwardSettings, UnifiedControlMode.VELOCITY, leftSetpoint) {
+                new MoveSmartMotorControllerGenericSubsystem(shooter.getLeftFlywheel(),
+                        shooter.getLeftFlywheel().pidSettings, shooter.getLeftFlywheel().feedForwardSettings,
+                        UnifiedControlMode.VELOCITY, leftSetpoint) {
                     @Override
                     public void end(boolean interrupted) {
                     }
                 },
-                new MoveSmartMotorControllerGenericSubsystem(shooter.getRightFlywheel(), shooter.getRightFlywheel().pidSettings,
-                        shooter.getRightFlywheel().feedForwardSettings, UnifiedControlMode.VELOCITY, rightSetpoint) {
+                new MoveSmartMotorControllerGenericSubsystem(shooter.getRightFlywheel(),
+                        shooter.getRightFlywheel().pidSettings, shooter.getRightFlywheel().feedForwardSettings,
+                        UnifiedControlMode.VELOCITY, rightSetpoint) {
                     @Override
                     public void end(boolean interrupted) {
                     }
