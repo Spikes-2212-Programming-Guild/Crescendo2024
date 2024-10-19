7492c7fbc02128e77e008a9d0ea79dc6817b518f
TurtleMinecraft <schnitzelgirl69@gmail.com>
2024-10-20 01:32:40 +0300

    bruh 32

--- src/main/java/frc/robot/commands/CloseIntake.java
+++ src/main/java/frc/robot/commands/CloseIntake.java
@@ -1,6 +1,6 @@
 package frc.robot.commands;
 
-import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.MoveSmartMotorControllerGenericSubsystem;
+import com.spikes2212.command.genericsubsystem.commands.smartmotorcontrollergenericsubsystem.*;
 import com.spikes2212.util.UnifiedControlMode;
 import frc.robot.subsystems.IntakePlacer;
 
