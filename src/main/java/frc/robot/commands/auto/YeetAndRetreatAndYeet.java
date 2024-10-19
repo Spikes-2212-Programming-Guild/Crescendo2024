28525683327824ac7bf29a937e5fc56665e772f3
TurtleMinecraft <schnitzelgirl69@gmail.com>
2024-10-20 01:22:06 +0300

    line break

--- src/main/java/frc/robot/commands/auto/YeetAndRetreatAndYeet.java
+++ src/main/java/frc/robot/commands/auto/YeetAndRetreatAndYeet.java
@@ -15,8 +15,8 @@ public class YeetAndRetreatAndYeet extends SequentialCommandGroup {
     private static final double MOVE_TIMEOUT = 1.5;
     private static final double DRIVE_SPEED = 1.0;
 
-    public YeetAndRetreatAndYeet(Drivetrain drivetrain, Shooter shooter, ShooterAdjuster adjuster, IntakePlacer intakePlacer,
-                                 IntakeRoller intakeRoller, Storage storage) {
+    public YeetAndRetreatAndYeet(Drivetrain drivetrain, Shooter shooter, ShooterAdjuster adjuster,
+                                 IntakePlacer intakePlacer, IntakeRoller intakeRoller, Storage storage) {
         addCommands(
                 new InstantCommand(intakePlacer::resetPosition),
                 adjuster.getResetCommand(),
@@ -29,7 +29,8 @@ public class YeetAndRetreatAndYeet extends SequentialCommandGroup {
                                 .withTimeout(MOVE_TIMEOUT),
                         new IntakeNote(intakeRoller, storage, intakePlacer, adjuster, false)
                 ),
-                new DriveSwerve(drivetrain, () -> -DRIVE_SPEED, () -> 0.0, () -> 0.0, false, false).withTimeout(MOVE_TIMEOUT),
+                new DriveSwerve(drivetrain, () -> -DRIVE_SPEED, () -> 0.0, () -> 0.0, false,
+                        false).withTimeout(MOVE_TIMEOUT),
                 new ParallelCommandGroup(
                         new Shoot(shooter, drivetrain, adjuster, storage, Shoot.CLOSE_HEIGHT).getCommand(),
                         new CloseIntake(intakePlacer)
